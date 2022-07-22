f26dd87bf5c278ec5c0a82325797a433876a5fe1
sehee <seeeh0128@gmail.com>
2022-07-22 20:34:29 +0900

    Design: 식단 모달 위치 수정

--- client/src/components/MealsList/MealsListDeleteModal/style.ts
+++ client/src/components/MealsList/MealsListDeleteModal/style.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  position: relative;
   width: 100%;
   height: 100%;
 `;
@@ -16,10 +17,12 @@ export const OutsideModal = styled.div`
 `;
 
 export const ModalContainer = styled.div`
+  position: absolute;
+  top: 220px;
   display: flex;
   flex-direction: column;
   height: 140px;
-  width: 60%;
+  width: 200px;
   background-color: white;
   border-radius: 15px;
   z-index: 2;
